a342db222b45b3e2258c356c0c54d82982ec45e6
YarosMJO <myhajlivej@gmail.com>
2018-07-30 12:55:31 +0300

    fixed

--- UwpClient/ViewModels/FlightsViewModel.cs
+++ UwpClient/ViewModels/FlightsViewModel.cs
@@ -1,24 +1,72 @@
 ﻿using AirportWebApi.DAL.Models;
 using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
 using GalaSoft.MvvmLight.Views;
 using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Threading.Tasks;
+using System.Windows.Input;
 using UwpClient.Services;
 
 namespace UwpClient.ViewModels
 {
-    public class FlightsViewModel : ViewModelBase
+    public class FlightsViewModel : ViewModelBase, INotifyPropertyChanged
     {
+        Flight entity;
+        Flight selected;
         FlightService flightservice;
+        ObservableCollection<Flight> flights;
+
         INavigationService navService;
 
-        ObservableCollection<Flight> flights;
+        public ICommand NewEntity { get; private set; }
+        public ICommand AddEntity { get; private set; }
+        public ICommand UpdateEntity { get; private set; }
+        public ICommand DeleteEntity { get; private set; }
+
+        public FlightsViewModel(INavigationService navService)
+        {
+            NewEntity = new RelayCommand(New);
+            AddEntity = new RelayCommand(Create);
+            UpdateEntity = new RelayCommand(Update);
+            DeleteEntity = new RelayCommand(Delete);
+            flightservice = new FlightService();
+            this.navService = navService;
+            LoadEntity().ConfigureAwait(false);
+            Flight = new Flight();
+        }
+
+        void New()
+        {
+            Flight = new Flight();
+        }
+
+        async void Delete()
+        {
+            await flightservice.Delete(FlightId);
+            await LoadEntity().ConfigureAwait(false);
+        }
+
+        async void Update()
+        {
+            await flightservice.Update(Flight);
+            await LoadEntity().ConfigureAwait(false);
+        }
+
+        async void Create()
+        {
+            await flightservice.Create(Flight);
+            await LoadEntity().ConfigureAwait(false);
+        }
+
+        async Task LoadEntity()
+        {
+            Flights = new ObservableCollection<Flight>(await flightservice.GetAll());
+        }
+
         public ObservableCollection<Flight> Flights
         {
-            get
-            {
-                return flights;
-            }
+            get { return flights; }
             set
             {
                 flights = value;
@@ -26,20 +74,13 @@ namespace UwpClient.ViewModels
             }
         }
 
-        private Flight entity;
         public Flight Flight
         {
-            get
-            {
-                return entity;
-            }
+            get { return entity; }
             set
             {
                 entity = value;
-                if (Flight != null)
-                {
-                    FlightId = Flight.Id;
-                }
+                if (Flight != null) FlightId = Flight.Id;
                 RaisePropertyChanged(() => Flight);
             }
         }
@@ -55,7 +96,7 @@ namespace UwpClient.ViewModels
             }
         }
 
-        private Flight selected;
+
         public Flight SelectedFlight
         {
             get { return selected; }
@@ -63,30 +104,9 @@ namespace UwpClient.ViewModels
             {
                 selected = value;
                 Flight = selected;
-
                 RaisePropertyChanged(() => SelectedFlight);
             }
         }
-
-        async Task LoadEntity()
-        {
-            Flights = new ObservableCollection<Flight>();
-            foreach (var flight in await flightservice.GetAll())
-            {
-                Flights.Add(flight);
-            }
-        }
-
-        public FlightsViewModel(INavigationService navigationService)
-        {
-            navService = navigationService;
-            flightservice = new FlightService();
-            LoadEntity().ConfigureAwait(false);
-            Flight = new Flight();
-        }
-
-
-
 
     }
 }
