b31e439f296d19fa9e86efb1a16a3af0df6fe9aa
YarosMJO <myhajlivej@gmail.com>
2018-07-30 18:06:51 +0300

    fixed

--- UwpClient/ViewModels/PlaneTypesViewModel.cs
+++ UwpClient/ViewModels/PlaneTypesViewModel.cs
@@ -1,9 +1,114 @@
-﻿using GalaSoft.MvvmLight;
+﻿using AirportWebApi.DAL.Models;
+using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
+using GalaSoft.MvvmLight.Views;
+using System.Collections.ObjectModel;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using UwpClient.Services;
 
 namespace UwpClient.ViewModels
 {
     public class PlaneTypesViewModel : ViewModelBase
     {
+        PlaneType entity;
+        PlaneType selected;
+        PlaneTypeService PlaneTypeservice;
+        ObservableCollection<PlaneType> planeTypes;
 
+        INavigationService navService;
+
+        public ICommand NewEntity { get; private set; }
+        public ICommand AddEntity { get; private set; }
+        public ICommand UpdateEntity { get; private set; }
+        public ICommand DeleteEntity { get; private set; }
+
+        public PlaneType SelectedPlaneType
+        {
+            get { return selected; }
+            set
+            {
+                selected = value;
+                PlaneType = selected;
+                RaisePropertyChanged(() => SelectedPlaneType);
+            }
+        }
+
+
+        public PlaneTypesViewModel(INavigationService navigationService)
+        {
+            PlaneTypeservice = new PlaneTypeService();
+            navService = navigationService;
+
+            NewEntity = new RelayCommand(New);
+            AddEntity = new RelayCommand(Create);
+            UpdateEntity = new RelayCommand(Update);
+            DeleteEntity = new RelayCommand(Delete);
+
+            LoadEntity().ConfigureAwait(false);
+            PlaneType = new PlaneType();
+        }
+
+
+        void New()
+        {
+            PlaneType = new PlaneType();
+        }
+
+        async void Create()
+        {
+            await PlaneTypeservice.Create(PlaneType);
+            await LoadEntity().ConfigureAwait(false);
+        }
+
+        async void Update()
+        {
+            await PlaneTypeservice.Update(PlaneType);
+            await LoadEntity().ConfigureAwait(false);
+        }
+
+        async void Delete()
+        {
+            await PlaneTypeservice.Delete(PlaneType.Id);
+            PlaneType = new PlaneType();
+            await LoadEntity().ConfigureAwait(false);
+        }
+
+        async Task LoadEntity()
+        {
+            planeTypes = new ObservableCollection<PlaneType>(await PlaneTypeservice.GetAll());
+        }
+
+        public ObservableCollection<PlaneType> PlaneTypes
+        {
+            get { return planeTypes; }
+            set
+            {
+                planeTypes = value;
+                RaisePropertyChanged(() => PlaneTypes);
+            }
+        }
+
+        public PlaneType PlaneType
+        {
+            get { return entity; }
+            set
+            {
+                entity = value;
+                if (PlaneType != null) PlaneTypeId = PlaneType.Id;
+                RaisePropertyChanged(() => PlaneType);
+            }
+        }
+
+        int entityId = 0;
+        public int PlaneTypeId
+        {
+            get { return entityId; }
+            set
+            {
+                entityId = value;
+                RaisePropertyChanged(() => PlaneTypeId);
+            }
+        }
     }
 }
--- UwpClient/ViewModels/PlanesViewModel.cs
+++ UwpClient/ViewModels/PlanesViewModel.cs
@@ -1,9 +1,114 @@
-﻿using GalaSoft.MvvmLight;
+﻿using AirportWebApi.DAL.Models;
+using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
+using GalaSoft.MvvmLight.Views;
+using System.Collections.ObjectModel;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using UwpClient.Services;
 
 namespace UwpClient.ViewModels
 {
     public class PlanesViewModel : ViewModelBase
     {
+        Plane entity;
+        Plane selected;
+        PlaneService Planeservice;
+        ObservableCollection<Plane> planes;
 
+        INavigationService navService;
+
+        public ICommand NewEntity { get; private set; }
+        public ICommand AddEntity { get; private set; }
+        public ICommand UpdateEntity { get; private set; }
+        public ICommand DeleteEntity { get; private set; }
+
+        public Plane SelectedPlane
+        {
+            get { return selected; }
+            set
+            {
+                selected = value;
+                Plane = selected;
+                RaisePropertyChanged(() => SelectedPlane);
+            }
+        }
+
+
+        public PlanesViewModel(INavigationService navigationService)
+        {
+            Planeservice = new PlaneService();
+            navService = navigationService;
+
+            NewEntity = new RelayCommand(New);
+            AddEntity = new RelayCommand(Create);
+            UpdateEntity = new RelayCommand(Update);
+            DeleteEntity = new RelayCommand(Delete);
+
+            LoadEntity().ConfigureAwait(false);
+            Plane = new Plane();
+        }
+
+
+        void New()
+        {
+            Plane = new Plane();
+        }
+
+        async void Create()
+        {
+            await Planeservice.Create(Plane);
+            await LoadEntity().ConfigureAwait(false);
+        }
+
+        async void Update()
+        {
+            await Planeservice.Update(Plane);
+            await LoadEntity().ConfigureAwait(false);
+        }
+
+        async void Delete()
+        {
+            await Planeservice.Delete(Plane.Id);
+            Plane = new Plane();
+            await LoadEntity().ConfigureAwait(false);
+        }
+
+        async Task LoadEntity()
+        {
+            Planes = new ObservableCollection<Plane>(await Planeservice.GetAll());
+        }
+
+        public ObservableCollection<Plane> Planes
+        {
+            get { return planes; }
+            set
+            {
+                planes = value;
+                RaisePropertyChanged(() => Planes);
+            }
+        }
+
+        public Plane Plane
+        {
+            get { return entity; }
+            set
+            {
+                entity = value;
+                if (Plane != null) PlaneId = Plane.Id;
+                RaisePropertyChanged(() => Plane);
+            }
+        }
+
+        int entityId = 0;
+        public int PlaneId
+        {
+            get { return entityId; }
+            set
+            {
+                entityId = value;
+                RaisePropertyChanged(() => PlaneId);
+            }
+        }
     }
 }
