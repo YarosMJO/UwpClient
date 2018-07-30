e840014991ba1071829130259ed74dc6687bf339
YarosMJO <myhajlivej@gmail.com>
2018-07-30 18:37:45 +0300

    fixed

--- UwpClient/ViewModels/CrewsViewModel.cs
+++ UwpClient/ViewModels/CrewsViewModel.cs
@@ -1,9 +1,114 @@
-﻿using GalaSoft.MvvmLight;
+﻿using AirportWebApi.DAL.Models;
+using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
+using GalaSoft.MvvmLight.Views;
+using System.Collections.ObjectModel;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using UwpClient.Services;
 
 namespace UwpClient.ViewModels
 {
     public class CrewsViewModel : ViewModelBase
     {
+        Crew entity;
+        Crew selected;
+        CrewService Crewservice;
+        ObservableCollection<Crew> crews;
 
+        INavigationService navService;
+
+        public ICommand NewEntity { get; private set; }
+        public ICommand AddEntity { get; private set; }
+        public ICommand UpdateEntity { get; private set; }
+        public ICommand DeleteEntity { get; private set; }
+
+        public Crew SelectedCrew
+        {
+            get { return selected; }
+            set
+            {
+                selected = value;
+                Crew = selected;
+                RaisePropertyChanged(() => SelectedCrew);
+            }
+        }
+
+
+        public CrewsViewModel(INavigationService navigationService)
+        {
+            Crewservice = new CrewService();
+            navService = navigationService;
+
+            NewEntity = new RelayCommand(New);
+            AddEntity = new RelayCommand(Create);
+            UpdateEntity = new RelayCommand(Update);
+            DeleteEntity = new RelayCommand(Delete);
+
+            LoadEntity().ConfigureAwait(false);
+            Crew = new Crew();
+        }
+
+
+        void New()
+        {
+            Crew = new Crew();
+        }
+
+        async void Create()
+        {
+            await Crewservice.Create(Crew);
+            await LoadEntity().ConfigureAwait(false);
+        }
+
+        async void Update()
+        {
+            await Crewservice.Update(Crew);
+            await LoadEntity().ConfigureAwait(false);
+        }
+
+        async void Delete()
+        {
+            await Crewservice.Delete(Crew.Id);
+            Crew = new Crew();
+            await LoadEntity().ConfigureAwait(false);
+        }
+
+        async Task LoadEntity()
+        {
+            Crews = new ObservableCollection<Crew>(await Crewservice.GetAll());
+        }
+
+        public ObservableCollection<Crew> Crews
+        {
+            get { return crews; }
+            set
+            {
+                crews = value;
+                RaisePropertyChanged(() => Crews);
+            }
+        }
+
+        public Crew Crew
+        {
+            get { return entity; }
+            set
+            {
+                entity = value;
+                if (Crew != null) CrewId = Crew.Id;
+                RaisePropertyChanged(() => Crew);
+            }
+        }
+
+        int entityId = 0;
+        public int CrewId
+        {
+            get { return entityId; }
+            set
+            {
+                entityId = value;
+                RaisePropertyChanged(() => CrewId);
+            }
+        }
     }
 }
--- UwpClient/ViewModels/DeparturesViewModel.cs
+++ UwpClient/ViewModels/DeparturesViewModel.cs
@@ -1,9 +1,114 @@
-﻿using GalaSoft.MvvmLight;
+﻿using AirportWebApi.DAL.Models;
+using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
+using GalaSoft.MvvmLight.Views;
+using System.Collections.ObjectModel;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using UwpClient.Services;
 
 namespace UwpClient.ViewModels
 {
     public class DeparturesViewModel : ViewModelBase
     {
+        Departure entity;
+        Departure selected;
+        DepartureService Departureservice;
+        ObservableCollection<Departure> departures;
 
+        INavigationService navService;
+
+        public ICommand NewEntity { get; private set; }
+        public ICommand AddEntity { get; private set; }
+        public ICommand UpdateEntity { get; private set; }
+        public ICommand DeleteEntity { get; private set; }
+
+        public Departure SelectedDeparture
+        {
+            get { return selected; }
+            set
+            {
+                selected = value;
+                Departure = selected;
+                RaisePropertyChanged(() => SelectedDeparture);
+            }
+        }
+
+
+        public DeparturesViewModel(INavigationService navigationService)
+        {
+            Departureservice = new DepartureService();
+            navService = navigationService;
+
+            NewEntity = new RelayCommand(New);
+            AddEntity = new RelayCommand(Create);
+            UpdateEntity = new RelayCommand(Update);
+            DeleteEntity = new RelayCommand(Delete);
+
+            LoadEntity().ConfigureAwait(false);
+            Departure = new Departure();
+        }
+
+
+        void New()
+        {
+            Departure = new Departure();
+        }
+
+        async void Create()
+        {
+            await Departureservice.Create(Departure);
+            await LoadEntity().ConfigureAwait(false);
+        }
+
+        async void Update()
+        {
+            await Departureservice.Update(Departure);
+            await LoadEntity().ConfigureAwait(false);
+        }
+
+        async void Delete()
+        {
+            await Departureservice.Delete(Departure.Id);
+            Departure = new Departure();
+            await LoadEntity().ConfigureAwait(false);
+        }
+
+        async Task LoadEntity()
+        {
+            Departures = new ObservableCollection<Departure>(await Departureservice.GetAll());
+        }
+
+        public ObservableCollection<Departure> Departures
+        {
+            get { return departures; }
+            set
+            {
+                departures = value;
+                RaisePropertyChanged(() => Departures);
+            }
+        }
+
+        public Departure Departure
+        {
+            get { return entity; }
+            set
+            {
+                entity = value;
+                if (Departure != null) DepartureId = Departure.Id;
+                RaisePropertyChanged(() => Departure);
+            }
+        }
+
+        int entityId = 0;
+        public int DepartureId
+        {
+            get { return entityId; }
+            set
+            {
+                entityId = value;
+                RaisePropertyChanged(() => DepartureId);
+            }
+        }
     }
 }
